7022e8fa55be8cce55a63d865652c10c5f8d0423
Akshara Sundararajan <49100882+akshara-sun@users.noreply.github.com>
2022-02-23 15:57:16 -0800

    initial commit

--- src/components/post-its.js
+++ src/components/post-its.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function PostIts() {
+  const [text, handleTextChange] = useState("");
+
+  const handleChange = (e) => {
+    handleTextChange(e.target.value);
+    localStorage.setItem("inputValue", e.target.value);
+  };
+
+  useEffect(() => {
+    handleTextChange(localStorage.getItem("inputValue"));
+  }, []);
+
   return (
     <ul className="post-its">
       <li>
-        <p>Post-Its</p>
+        <div className="button-container">
+          <button id="close-button">x</button>
+        </div>
+        <p>
+          <input
+            id="sticky-text"
+            placeholder="Enter text here..."
+            cols="25"
+            rows="10"
+            value={text}
+            style={{ fontFamily: "Gloria Hallelujah, cursive" }}
+            onChange={handleChange}
+          />
+        </p>
       </li>
     </ul>
   );
